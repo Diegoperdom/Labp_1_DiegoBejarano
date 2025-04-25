7a7c4fb782041696580f6aa87686c79f9cefe116
Diego Bejarano <diegoalejandrop78gmail.com>
2025-04-25 14:06:52 -0600

    Inicial

---
+++ src/labp_1_diegobejarano/Labp_1_DiegoBejarano.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package labp_1_diegobejarano;
+
+/**
+ *
+ * @author diego
+ */
+public class Labp_1_DiegoBejarano {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
